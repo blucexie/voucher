951341c3f3e955bf71e9b7132b3c6f61b0a99953
谢玉琪 <649453836@qq.com>
2018-06-01 18:21:35 +0800

    UI初步完成

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.redeemNow').click(function(){
+        $(".shade").show();
+          $(".success")
+            .addClass("animated bounceInDown")
+            .show();
+    })
+
+})
